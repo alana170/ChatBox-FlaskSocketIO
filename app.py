8e919050aa9447f99cf409e65931b976e63fff50
alana170 <52054877+alana170@users.noreply.github.com>
2020-07-27 19:50:25 -0400

    edits

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+import os, time
 from flask import Flask, render_template, request, url_for
+from collections import deque
+from flask_socketio import emit, SocketIO
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+socketio = SocketIO(app)
+
+USERS = {}
+CHANNELS = {"Public": {"messages": deque([], maxlen=100), "username": "default"}}    
+
 
 @app.route('/', methods = ['GET', 'POST'])
 def index() : 
